22e86f1b19c1dc518fad24b8356c5d53a660dff4
wimsikal <danny@luedkes.com>
2021-03-22 07:40:27 -0700

    feat: tickets

--- components/ticket-info.tsx
+++ components/ticket-info.tsx
@@ -26,7 +26,7 @@ const siteUrlForTicket = `${siteUrl.host}${siteUrl.pathname}`.replace(/\/$/, '')
 export default function TicketInfo({ logoTextSecondaryColor = 'var(--accents-5)' }) {
   const createdBy = (
     <div className={styles['created-by']}>
-      <div className={styles['created-by-text']}>Created by </div>
+      <div className={styles['created-by-text']}>Hosted by </div>
       <div className={styles['created-by-logo']}>
         <Nobl9Logo />
       </div>
