36fff86f43355e575285e5b38c6aea4b6a9574de
wimsikal <danny@luedkes.com>
2021-05-16 21:22:28 -0700

    new

--- pages/stage/[slug].tsx
+++ pages/stage/[slug].tsx
@@ -31,7 +31,7 @@ type Props = {
 
 export default function StagePage({ stage, allStages }: Props) {
   const meta = {
-    title: 'Demo - Virtual Event Starter Kit',
+    title: 'SLOconf',
     description: META_DESCRIPTION
   };
 
